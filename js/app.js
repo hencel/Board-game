ac84b35af2aeef11371c5c3d101e995f91334942
Zbigniew Piernik <zbigniewpiernik@gmail.com>
2019-06-17 23:21:40 +0200

    Corrected the way to drawwing a board and drawing the path for game.

--- js/app.js
+++ js/app.js
@@ -1,74 +1,66 @@
 import "../scss/main.scss";
 import { throws } from "assert";
+import { pathToFileURL } from "url";
 
 document.addEventListener('DOMContentLoaded', function() {
+    let counter = 0;
     class Generator {
         constructor() {
             this.width = 10;
             this.height = 10;
         };
         createBoard() {
-            let array = [];
+            let gamePath = [];
+            for (let i=0; i<this.width*this.height; i++) {
+                let newDiv = document.createElement('div');
+                document.getElementById("board").appendChild(newDiv);
+                newDiv.className = "cell";
+                newDiv.classList.add('potentialActive');
+                newDiv.setAttribute('data-id',i);
+                
+                newDiv.addEventListener('click', function() {
+                    if(this.classList.contains('potentialActive')){
+                        let numberingCells = this.getAttribute('data-id');
+                        console.log(numberingCells);
+                        this.classList.add('active');
 
-            for (let x=0; x<=10; x++) {
-                array[x] = [];
-                for(let y=0; y<10; y++) {
-                    let newDiv = document.createElement('div');
-                    array[x][y] = newDiv;
-                    newDiv.className = "cell";
-                    newDiv.setAttribute("x",x);
-                    newDiv.setAttribute("y",y);
-                    document.getElementById("board").appendChild(newDiv);  
-                }
+                        gamePath.push(this.getAttribute('data-id'));
+                        this.setAttribute('data-counter',counter);
+                        counter++;
+                        this.classList.remove('potentialActive');
+
+                        let potentialList = document.querySelectorAll('div.potentialActive');
+                        for(let j=0; j<potentialList.length; j++) {
+                            potentialList[j].classList.remove('potentialActive');
+                        }
+                        let numberingCellsminus10 = document.querySelector('div[data-id="'+(numberingCells-10)+'"]');
+                        if(!numberingCellsminus10.classList.contains('active') ) {
+                            numberingCellsminus10.classList.add('potentialActive');
+                        }
+                        let numberingCellsplus10 = document.querySelector('div[data-id="'+(parseInt(numberingCells)+10)+'"]');
+                        if(!numberingCellsplus10.classList.contains('active') ) {
+                            numberingCellsplus10.classList.add('potentialActive');
+                        }
+                        let numberingCellsplus1 = document.querySelector('div[data-id="'+(parseInt(numberingCells)+1)+'"]');
+                        if(!numberingCellsplus1.classList.contains('active') && (parseInt(numberingCells)+1)%10!=0) {
+                            numberingCellsplus1.classList.add('potentialActive');
+                        }
+                        let numberingCellsminus1 = document.querySelector('div[data-id="'+(parseInt(numberingCells)-1)+'"]');
+                        if(!numberingCellsminus1.classList.contains('active') && (parseInt(numberingCells)-1)%10!=9) {
+                            numberingCellsminus1.classList.add('potentialActive');
+                        }
+                        console.log((parseInt(numberingCells)-1)%10);
+                    }
+                    this.classList.remove('potentialActive');
+                })
             }
-            let something = document.querySelectorAll('.cell');
-            console.log(something);
-            console.log(array[0][3]);
-
-            for(let i=0; i<=something.length; i++) {
-                something[i].addEventListener('click', function() {
-                    let x = this.getAttribute('x');
-                    let y = this.getAttribute('y');
-                    
-                    //tutaj wpisać całą logikę
-
-                });
-            }
-
-            // for(let i=0; i<array.length; i++) {
-            //     for(let j=0; j<array[i].length; j++) {
-                    // array[i][j].addEventListener('click', function() { 
-                    //     console.log('działa');
-                    //     if (this.classList.contains('active') ) {
-                    //         console.log(".");
-                    //     } else {
-                    //         this.classList.add('active');
-                    //         if(array[i+1][j].classList.contains('active') && array[i-1][j].classList.contains('active') && array[i][j+1].classList.contains('active') && array[i][j-1].classList.contains('active')) {
-                    //             console.log('nic');    
-                    //         } else { //tutaj wstawić sprawdzenie czy zawiera dla każdego pola indywidualnie
-                    //             if (this.classList.contains('active') ) {
-                    //                 console.log(".");
-                    //             } else 
-                    //             array[i+1][j].classList.add('potentialActive');
-                    //             array[i-1][j].classList.add('potentialActive');
-                    //             array[i][j+1].classList.add('potentialActive');
-                    //             array[i][j-1].classList.add('potentialActive');
-
-                    //         }
-                    //     }
-                    // })
-
-            //     }    
-            // }
-
+            console.log(gamePath)
         }
     }
 
     let generator = new Generator();
     generator.createBoard();
 
-    //blokada przed kliknięciem gdziekolwiek - tylko sąsiednie pola - dodanie klasy potentialActive
-    //zapisanie współrzędnych klikniętych pól do tablicy
     //ustawienie napisów Start, Koniec na pierwszym i ostatnim polu ścieżki
     //randomowe przypisanie funkcji pod pola ścieżki, poza startem i końcem
     //kostka do gry
